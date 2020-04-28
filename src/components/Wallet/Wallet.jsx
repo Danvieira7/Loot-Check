8a2759e91eafe25edeb086acd784ee3a58b29fa2
“Danvieira7” <danillovieira7@gmail.com>
2020-04-28 15:58:08 -0700

    implement tested feature of the deposit function

--- src/components/Wallet/Wallet.jsx
+++ src/components/Wallet/Wallet.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { deposit } from "../../actions/balance";
 
 // don't export Wallet class as default 
 export class Wallet extends Component {
@@ -16,15 +17,19 @@ export class Wallet extends Component {
         });
     }
 
+    deposit = () => this.props.deposit(this.state.balance);
+    
+
     render() {
         return (
             <div>
                 <h3 className="balance">Wallet balance: { this.props.balance }</h3>
                 <br />
                 <input className="input-wallet" onChange={this.updateBalance} />
+                <button className="btn-deposit" onClick={this.deposit}>deposit</button>
             </div>
         )
     }
 }
 
-export default connect(state => { return { balance: state } }, null)(Wallet);
+export default connect(state => { return { balance: state } }, { deposit })(Wallet);
