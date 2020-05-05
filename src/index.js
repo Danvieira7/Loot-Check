60985c5ad5fc9ff7110df5f7e03b29876f986858
“Danvieira7” <danillovieira7@gmail.com>
2020-05-05 11:34:56 -0700

    implement a new bitcoin reducer and thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore } from "redux";
-import rootReducer from "./reducers/balance";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 import { Provider } from"react-redux";
 import App from "./pages/App/App";
 
-// create store
-const store = createStore(rootReducer);
-
 // distribute the store to the app in it's higher component
 render(
-    <Provider store={store}>
+    <Provider store={ createStore(rootReducer, applyMiddleware(thunk))}>
         <App />
     </Provider>, 
     document.getElementById("root")
